b30ae585a2f4dad71be86a22432a256bfe2c68a2
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2026-04-21 07:56:20 +0900

    feat: 申請詳細取得APIのクライアント処理を追加

--- src/api/applicationsApi.ts
+++ src/api/applicationsApi.ts
@@ -1,8 +1,21 @@
-import type { ApplicationListItem } from "../types/application";
+import type {
+  ApplicationDetail,
+  ApplicationListItem,
+} from "../types/application";
 import apiClient from "./apiClient";
 
-// アプリケーションの一覧を取得する関数
+// 申請一覧を取得する関数
 export async function getApplications(): Promise<ApplicationListItem[]> {
   const response = await apiClient.get<ApplicationListItem[]>("/applications");
+  return response.data;
+}
+
+// 申請詳細を取得する関数
+export async function getApplicationDetail(
+  id: number,
+): Promise<ApplicationDetail> {
+  const response = await apiClient.get<ApplicationDetail>(
+    `/applications/${id}`,
+  );
   return response.data;
 }
--- src/types/application.ts
+++ src/types/application.ts
@@ -1,6 +1,14 @@
 export type ApplicationListItem = {
   id: number;
   title: string;
+  content: string; // 削除予定
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type ApplicationDetail = {
+  id: number;
+  title: string;
   content: string;
   createdAt: string;
   updatedAt: string;
