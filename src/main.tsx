64c5b302b62aa3cd9f0f7206c03d4619ba30ad48
Seiya Sakaguchi <s4e2i4ya@gmail.com>
2026-08-02 15:21:32 +0900

    feat: React Query Devtoolsを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import "./index.css";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -11,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
